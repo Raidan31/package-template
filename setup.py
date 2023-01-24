f29a0da4505e302171486bd1e259e13924a07c5c
Raidan <114839023+Raidan31@users.noreply.github.com>
2023-01-24 20:36:03 -0300

    Processador de Imagem

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="Image Processing",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Raidan Nunes de Oliveira",
+    author_email="raidan.nunes31@gmail.com",
+    description="Pacote de Processamento de Imagem",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/Raidan31/package-template.git",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
